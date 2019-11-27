370af6182af80261656ec8630b4d3d67eae414e4
Mohammed Hossam <31958366+MohammedHossam@users.noreply.github.com>
2019-11-27 22:32:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,50 @@
 
-#include <mpi.h>
+//---------------------------------------------------Dynamic Array-------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
+#include <mpi.h>
+#include <assert.h>
 
 #define MAXCHAR 10000
 #define MAXWORDLENGTH 100
+
 //---------------------------------------------------Dynamic Array-------------------------------------------------------
+struct data
+{
+    char word_string[100];
+};
 typedef struct
 {
-    char **array;
+    struct data *array;
     size_t used;
     size_t size;
 } Array;
 
 void initArray(Array *a, size_t initialSize)
 {
-    a->array = malloc(initialSize * sizeof(char *));
+    a->array = malloc(initialSize * 100);
     a->used = 0;
     a->size = initialSize;
 
-    for (int i = 0; i < initialSize; i++)
-        a->array[i] = malloc((MAXCHAR + 1) * sizeof(char));
+    // for (int i = 0; i < initialSize; i++)
+    //     a->array[i] = malloc(100);
 }
 
 void insertArray(Array *a, char *element)
 {
+    struct data el;
+    strcpy(el.word_string, element);
     if (a->used == a->size)
     {
         a->size *= 2;
-        a->array = realloc(a->array, a->size * sizeof(char *));
-        for (int i = a->used; i < a->size; i++)
-            a->array[i] = malloc((MAXCHAR + 1) * sizeof(char));
+        a->array = realloc(a->array, a->size * 100);
+        // for (int i = a->used; i < a->size; i++)
+        //     a->array[i] = malloc((MAXCHAR + 1) * sizeof(char));
     }
-    strcpy(a->array[a->used++], element);
+    // strcpy(a->array[a->used++], element);
+    a->array[a->used++] = el;
 }
 
 void freeArray(Array *a)
@@ -72,65 +83,68 @@ int readAndSplit(char *filename)
     return 0;
 }
 
+void search()
+{
+    MPI_Init(NULL, NULL);
+    int my_id;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    int num_proc;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
+    char target[100];
+    int result;
+    //-------------------------------------defining the struct--------------------------------------
+    const int nitems = 1;
+    int blocklengths[1] = {100};
+    MPI_Datatype types[1] = {MPI_CHAR};
+    MPI_Datatype mpi_string_type;
+    MPI_Aint offsets[1];
+
+    offsets[0] = offsetof(struct data, word_string);
+
+    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_string_type);
+    MPI_Type_commit(&mpi_string_type);
+    //---------------------------------------------------------------------------
+    char *filename = "./a.txt";
+    readAndSplit(filename);
+
+    if (my_id == 0)
+    {
+        puts("Please enter a word (We are Case-Sensetive :) ) : ");
+        scanf("%s", &target);
+    }
+    MPI_Bcast(&target, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    int proc_subArray = a.used / (num_proc);
+    struct data recv[proc_subArray];
+
+    MPI_Scatter(a.array, proc_subArray, mpi_string_type, &recv, proc_subArray, mpi_string_type, 0, MPI_COMM_WORLD);
+    int partial_sum = 0;
+
+    for (int i = 0; i < proc_subArray; i++)
+    {
+        if (strcmp(recv[i].word_string, target) == 0)
+            partial_sum++;
+    }
+
+    if (my_id == 0)
+    {
+        int start = proc_subArray * num_proc;
+        for (int i = start; i < a.used; i++)
+        {
+            if (strcmp(a.array[i].word_string, target) == 0)
+                partial_sum++;
+        }
+    }
+
+    MPI_Reduce(&partial_sum, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (my_id == 0)
+    {
+        printf("the word has appeared %i times\n", result);
+    }
+    MPI_Finalize();
+}
+
 int main(int argc, char **argv)
 {
-
-    char *filename = "./a.txt";
-    if (readAndSplit(filename) == 1)
-    {
-        return 1;
-    }
-
-    else
-    {
-        MPI_Init(&argc, &argv);
-        int result = 0;
-        // Get the number of processes
-        int num_proc;
-        MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
-        // Get the rank of the process
-        int my_id;
-        MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-
-        char target[100];
-        if (my_id == 0)
-        {
-            puts("Please enter a word (We are Case-Sensetive :) ) : ");
-            scanf("%s", &target);
-        }
-        MPI_Bcast(&target, 100, MPI_CHAR, 0,
-                  MPI_COMM_WORLD);
-        int partial_sum = 0;
-        int proc_subArray = a.used / (num_proc - 1);
-        if (my_id != 0)
-        {
-            int proc_start = (my_id - 1) * proc_subArray;
-            int proc_end = proc_start + proc_subArray;
-            for (int i = proc_start; i < proc_end; i++)
-            {
-                if (strcmp(a.array[i], target) == 0)
-                    partial_sum++;
-            }
-        }
-        else
-        {
-            partial_sum = 0;
-            int remaining = a.used % (num_proc - 1);
-            int last_proc_start = (num_proc - 2) * proc_subArray;
-            int last_proc_end = last_proc_start + proc_subArray;
-
-            for (int i = last_proc_end; i < last_proc_end + remaining; i++)
-            {
-                if (strcmp(a.array[i], target) == 0)
-                    partial_sum++;
-            }
-        }
-        MPI_Reduce(&partial_sum, &result, 1, MPI_INT,
-                   MPI_SUM, 0, MPI_COMM_WORLD);
-        if (my_id == 0)
-        {
-            printf("the word has appeared %i times\n", result);
-        } // Finalize the MPI environment.
-        MPI_Finalize();
-    }
+    search();
 }
